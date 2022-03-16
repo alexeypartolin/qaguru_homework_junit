3727cb9720d7bc7850a6223d6f3010898c289ae7
alexeypartolin <ivanovlesha1337@gmail.com>
2022-03-17 01:23:43 +0300

    TestBase.java class added

---
+++ src/test/java/com/alexeypartolin/tests/TestBase.java
@@ -0,0 +1,25 @@
+package com.alexeypartolin.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        //Configuration.browser = "firefox";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        Selenide.open("https://leroymerlin.kz/");
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
+    }
+}
